d7c14afdd30e200af25e58d834c5dce4a1c94436
Aravindan Lakshmanan <aravindanlakshmanan@ibm.com>
2025-12-29 00:18:45 +0530

    Repo layer with array storage was implemented

--- src/com/airtribe/learntrack/repository/CourseRepository.java
+++ src/com/airtribe/learntrack/repository/CourseRepository.java
@@ -1,5 +1,28 @@
 package com.airtribe.learntrack.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.airtribe.learntrack.entity.Course;
+
 public class CourseRepository {
-    
+
+    private final List<Course> courses = new ArrayList<>();
+
+    public void save(Course course) {
+        courses.add(course);
+    }
+
+    public Course findById(int id) {
+        for (Course course : courses) {
+            if (course.getId() == id) {
+                return course;
+            }
+        }
+        return null;
+    }
+
+    public List<Course> findAll() {
+        return courses;
+    }
 }
--- src/com/airtribe/learntrack/repository/StudentRepository.java
+++ src/com/airtribe/learntrack/repository/StudentRepository.java
@@ -1,4 +1,28 @@
 package com.airtribe.learntrack.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.airtribe.learntrack.entity.Student;
+
 public class StudentRepository {
+
+    private final List<Student> students = new ArrayList<>();
+
+    public void save(Student student) {
+        students.add(student);
+    }
+
+    public Student findById(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> findAll() {
+        return students;
+    }
 }
